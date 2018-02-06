d50540ca7e945955da09556b4c0c3d618ba9cfe1
Suket Sharma <sukets@amazon.com>
2018-02-06 14:59:51 -0800

    Creating new handler package

---
+++ lambdahandler/handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"io/ioutil"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// Handler takes in an APIGatewayProxyRequest, processes it,
+// and returns an APIGatewayProxyResponse.
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	index, err := ioutil.ReadFile("../public/index.html")
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(index),
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}, nil
+
+}
--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"io/ioutil"
-
-	"github.com/aws/aws-lambda-go/events"
+	lambdahandler "./lambdahandler"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Handler takes in an APIGatewayProxyRequest, processes it,
-// and returns an APIGatewayProxyResponse.
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(index),
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-	}, nil
-
-}
-
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(lambdahandler.Handler)
 }
